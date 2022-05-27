f2712c9f1fda4a708f26b3e3ddfb48702ced1c76
jisu <jisuuoo96@gmail.com>
2022-05-27 23:47:13 +0900

    feat : stack 직접 구현

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MyStack<T> {
+
+    private ArrayList<T> stack = new ArrayList<>();
+
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.remove(stack.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyStack<Integer> ms = new MyStack<Integer>();
+        ms.push(3);
+        ms.push(4);
+        System.out.println(ms.pop());
+    }
+
+}
