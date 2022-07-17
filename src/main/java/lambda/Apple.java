a59bd0352d36102f368572712465e6520c20b143
jisu <jisuuoo96@gmail.com>
2022-07-17 18:53:02 +0900

    feat : 2단계 색을 파라미터화

--- src/main/java/lambda/Apple.java
+++ src/main/java/lambda/Apple.java
@@ -3,6 +3,10 @@ package lambda;
 public class Apple {
     private Color color;
 
+    public Apple(Color color) {
+        this.color = color;
+    }
+
     public Color getColor() {
         return color;
     }
--- src/main/java/lambda/LambdaExample.java
+++ src/main/java/lambda/LambdaExample.java
@@ -1,20 +1,29 @@
 package lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static lambda.Color.GREEN;
+import static lambda.Color.RED;
 
 public class LambdaExample {
 
-    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+    public static List<Apple> filterGreenApples(List<Apple> inventory, Color color) {
         List<Apple> result = new ArrayList<>();
 
         for (Apple apple : inventory) {
-            if (GREEN.equals(apple.getColor())) {
+            if (apple.getColor().equals(color)) {
                 result.add(apple);
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(GREEN), new Apple(RED), new Apple(GREEN));
+
+        List<Apple> grrenApples = filterGreenApples(inventory, GREEN);
+        List<Apple> RedApples = filterGreenApples(inventory, RED);
     }
 }
